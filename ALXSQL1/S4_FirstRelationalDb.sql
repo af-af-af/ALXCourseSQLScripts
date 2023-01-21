b2cee2d10a2feacb21e7ab5a249abc02d9027ced
Andrzej Frydryszak <cne.studio.akk@gmail.com>
2023-01-21 15:46:44 +0100

    relational db

--- ALXSQL1/S4_FirstRelationalDb.sql
+++ ALXSQL1/S4_FirstRelationalDb.sql
@@ -19,3 +19,25 @@ create table employees
 	department_id uniqueidentifier not null foreign key references departments(id)
 )
 go
+
+insert into departments(id, department_name)
+values (NEWID(), 'HR'),
+	(NEWID(), 'IT'),
+	(NEWID(), 'PR'),
+	(NEWID(), 'R&D'),
+	(NEWID(), 'Office')
+go
+
+insert into employees(id, first_name, last_name, email, department_id)
+values(NEWID(), 'Tomasz', 'IeeeMacarena', 'a@kotamaala.com', 'FA50E322-A986-4F5D-8776-328E9A7E888A'),
+(NEWID(), 'Jacek', 'Aaa', 'aa@kotamaala.com', 'FA50E322-A986-4F5D-8776-328E9A7E888A'),
+(NEWID(), 'Piotr', 'BBB', 'bbb@kotamaala.com', '4656040C-EE24-44D1-98B6-C534E93CA2FA'),
+(NEWID(), 'Misio', 'Yya', 'pp@kotamaala.com', '886EEE76-1508-44A6-A172-C2A5255B9DA6')
+
+select * from departments
+select * from employees
+select * from employees inner join departments on employees.department_id=departments.id
+
+select department_name from departments
+select first_name, last_name from employees
+select first_name, last_name, department_name from employees inner join departments on employees.department_id=departments.id
